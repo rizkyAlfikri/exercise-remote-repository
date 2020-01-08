9422d668c51ca2fac6688950810a2a368ac346d3
rizkyAlfikri <rizkyalfikri@gmail.com>
2020-01-08 20:01:53 +0700

    add class savior and swordman

---
+++ exampleFile/test.kt
@@ -0,0 +1,22 @@
+abstract class Savior(){
+    abstract var str: Int
+    abstract var wiz: Int
+    abstract var def: Int
+    abstract var dex: Int
+
+}
+
+class Swordman : Savior {
+    override var str: Int
+        get() = println(str)
+        set(value) {value = 10}
+    override var wiz: Int
+        get() = println(wiz)
+        set(value) {value = 1}
+    override var def: Int
+        get() = println(def)
+        set(value) {value = 5}
+    override var dex: Int
+        get() = println(def)
+        set(value) {value = 3}
+}
